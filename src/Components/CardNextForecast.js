c2473893050a3476edb7551b9ad544ddc060fbbb
Ketlinl <ketlinlarissa37@gmail.com>
2023-04-27 19:15:36 -0300

    styling title and image

--- src/Components/CardNextForecast.js
+++ src/Components/CardNextForecast.js
@@ -4,54 +4,56 @@ import { View, Text, Image } from "react-native";
 const CardBoxForecast = (props) => {
   return (
     <Background>
-    <View style={{ flexDirection: "column", justifyContent: "space-between" }}>
+      <View
+        style={{ flexDirection: "column", justifyContent: "space-between" }}
+      >
         <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-            <Text>Next Forecast</Text>
-            <Text>Calendario</Text>
+          <TextTitle>Next Forecast</TextTitle>
+          <Image source={require("../../assets/calendar.png")} />
         </View>
         <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-            <Text>Monday</Text>
-            <Text>Imagem</Text>
-            <Text>13º</Text>
-            <Text>10º</Text>
+          <Text>Monday</Text>
+          <Text>Imagem</Text>
+          <Text>13º</Text>
+          <Text>10º</Text>
         </View>
         <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-            <Text>Monday</Text>
-            <Text>Imagem</Text>
-            <Text>13º</Text>
-            <Text>10º</Text>
+          <Text>Monday</Text>
+          <Text>Imagem</Text>
+          <Text>13º</Text>
+          <Text>10º</Text>
         </View>
         <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-            <Text>Monday</Text>
-            <Text>Imagem</Text>
-            <Text>13º</Text>
-            <Text>10º</Text>
+          <Text>Monday</Text>
+          <Text>Imagem</Text>
+          <Text>13º</Text>
+          <Text>10º</Text>
         </View>
         <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-            <Text>Monday</Text>
-            <Text>Imagem</Text>
-            <Text>13º</Text>
-            <Text>10º</Text>
+          <Text>Monday</Text>
+          <Text>Imagem</Text>
+          <Text>13º</Text>
+          <Text>10º</Text>
         </View>
         <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-            <Text>Monday</Text>
-            <Text>Imagem</Text>
-            <Text>13º</Text>
-            <Text>10º</Text>
+          <Text>Monday</Text>
+          <Text>Imagem</Text>
+          <Text>13º</Text>
+          <Text>10º</Text>
         </View>
         <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-            <Text>Monday</Text>
-            <Text>Imagem</Text>
-            <Text>13º</Text>
-            <Text>10º</Text>
+          <Text>Monday</Text>
+          <Text>Imagem</Text>
+          <Text>13º</Text>
+          <Text>10º</Text>
         </View>
         <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-            <Text>Monday</Text>
-            <Text>Imagem</Text>
-            <Text>13º</Text>
-            <Text>10º</Text>
+          <Text>Monday</Text>
+          <Text>Imagem</Text>
+          <Text>13º</Text>
+          <Text>10º</Text>
         </View>
-    </View>
+      </View>
     </Background>
   );
 };
@@ -61,6 +63,7 @@ const Background = styled.View`
   margin-left: 40px;
   margin-left: 40px;
   margin-right: 40px;
+  margin-bottom: 40px;
   flex-direction: column;
   justify-content: space-between;
   background-color: rgba(0, 16, 38, 0.3);
@@ -72,6 +75,14 @@ const Background = styled.View`
   border-bottom-right-radius: 20px;
   border-bottom-left-radius: 20px;
   padding: 14px 14px 30px 14px;
+`;
+
+const TextTitle = styled.Text`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 24px;
+  color: #ffffff;
 `;
 
 export default CardBoxForecast;
